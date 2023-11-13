463fe6fb6224192fa5f8bffc7bfe3b1b1a221224
Yassine <yassinemoussafir31@gmail.com>
2023-11-13 09:32:40 -0800

    flags

--- _printf.c
+++ _printf.c
@@ -43,23 +43,23 @@ int _printf(const char *format, ...)
 		}
 		else if (format[i] == '%' && format[i + 1] == 'S')
 		{
- 			   s = va_arg(args, char*);
-    			for (j = 0; s[j] != '\0'; j++) {
-       				 if ((s[j] > 0 && s[j] < 32) || s[j] >= 127)
-				 {
-          				  _putchar('\\');
-           				 _putchar('x');
-           				 charToHex(s[j]);
-        				 counter += 2;
-				 }
-				 else
-				 {
-           				 _putchar(s[j]);
-       				 }
-       				 counter++;
-    				}
-    				i++;
-		}	
+			s = va_arg(args, char*);
+			for (j = 0; s[j] != '\0'; j++) {
+				if ((s[j] > 0 && s[j] < 32) || s[j] >= 127)
+				{
+					_putchar('\\');
+					_putchar('x');
+					charToHex(s[j]);
+					counter += 2;
+				}
+				else
+				{
+					_putchar(s[j]);
+				}
+				counter++;
+			}
+			i++;
+		}
 		else if (format[i] == '%' && format[i + 1] == '%')
 		{
 			_putchar('%');
@@ -77,6 +77,36 @@ int _printf(const char *format, ...)
 				counter++;
 			}
 			i++;
+		}
+		if ((format[i] == '%' && format[i + 1] == '+' && format[i + 2] == 'd') ||
+			(format[i] == '%' && format[i + 1] == '+' && format[i + 2] == 'i')) {
+			di = va_arg(args, int);
+			if (di >= 0) {
+				_putchar('+');
+				counter++;
+			}
+			_itoa(di, str);
+			for (j = 0; str[j] != '\0'; j++) {
+				_putchar(str[j]);
+				counter++;
+			}
+			i += 2;
+		}
+		else if ((format[i] == '%' && format[i + 1] == ' ' && format[i + 2] == 'd')
+				|| (format[i] == '%' && format[i + 1] == ' ' && format[i + 2] == 'i'))
+		{
+			int num = va_arg(args, int);
+			if (num >= 0 && format[i - 1] != '+') {
+				_putchar(' ');
+				counter++;
+			}
+			_itoa(di, str);
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				_putchar(str[j]);
+				counter++;
+			}
+			i+= 2;
 		}
 		else if (format[i] == '%' && format[i + 1] == 'b')
 		{
@@ -97,28 +127,39 @@ int _printf(const char *format, ...)
 			}
 			i++;
 		}
-		else if (format[i] == '%' && format[i + 1] == 'o')
-		{
+		else if (format[i] == '%' && (format[i + 1] == 'o' || (format[i + 1] == '#' && format[i + 2] == 'o'))) {
 			unsigned int o = va_arg(args, unsigned int);
-
+			if (format[i + 1] == '#' && o != 0) {
+				_putchar('0');
+				_putchar('o');
+				counter += 2;
+			}
 			counter += decimalToOctal(o);
-			i++;
+			i += (format[i + 1] == '#') ? 3 : 2;
 		}
-		else if (format[i] == '%' && format[i + 1] == 'x')
-		{
+		else if (format[i] == '%' && (format[i + 1] == 'x' || (format[i + 1] == '#' && format[i + 2] == 'x'))) {
 			unsigned int x = va_arg(args, unsigned int);
+			if (format[i + 1] == '#' && x != 0) {
+				_putchar('0');
+				_putchar('x');
+				counter += 2;
+			}
 			counter += decimalToHexadecimalx(x);
-			i++;
+			i += (format[i + 1] == '#') ? 3 : 2;
 		}
-		else if (format[i] == '%' && format[i + 1] == 'X')
-		{
+		else if (format[i] == '%' && (format[i + 1] == 'X' || (format[i + 1] == '#' && format[i + 2] == 'X'))) {
 			unsigned int X = va_arg(args, unsigned int);
+			if (format[i + 1] == '#' && X != 0) {
+				_putchar('0');
+				_putchar('X');
+				counter += 2;
+			}
 			counter += decimalToHexadecimalX(X);
-			i++;
+			i += (format[i + 1] == '#') ? 3 : 2;
 		}
 		else if (format[i] == '%' && format[i + 1] == 'p')
 		{
-			void *p = va_arg(args, void*);
+			void *p = va_arg(args, void *);
 			print_address(p);
 			counter =+ print_address(p);
 			i++;
@@ -133,3 +174,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (counter);
 }
+
--- main.c
+++ main.c
@@ -9,36 +9,31 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+    int test_val = 123;
+    char *test_str = "Hello, World!";
+    void *ptr = &test_val;
+    unsigned int number = 123;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
+    _printf("Number as octal: %o\n", number);
+    _printf("Number as octal with #: %#o\n", number);
 
-    _printf("%S\n", "Best\nSchool");
+    _printf("Number as hexadecimal : %x\n", number);
+    _printf("Number as hexadecimal with #: %#x\n", number);
+
+    _printf("Number as HEXADECIMAL: %X\n", number);
+    _printf("Number as HEXADECIMAL with #: %#X\n", number);
+    _printf("Test string: %s\n", test_str);
+
+    _printf("Test integer: %+d\n", test_val);
+    _printf("Test integer: %+i\n", test_val);
+    _printf("Test integer: %d\n", -20);
+
+    _printf("Test pointer: %p\n", ptr);
+    printf("Number with + flag: %+d\n", 42);
+    _printf("Number with + flag: %+d\n", -42);
+    _printf("Number with + flag: %+d\n", 0);
+
+    printf("Standard printf - Test pointer: %p\n", ptr);
     return (0);
 }
+
