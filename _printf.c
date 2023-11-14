eeaebbb713ea71c62598e89333a51bb8102d2487
Yassine <yassinemoussafir31@gmail.com>
2023-11-14 04:10:11 -0800

    fix

--- _printf.c
+++ _printf.c
@@ -11,65 +11,39 @@
  *              It prints to the standard output.
  * Return: Number of characters printed (excluding the null byte)
  */
+int print_char(char c) {
+	return _putchar(c);
+}
+
 int _printf(const char *format, ...)
 {
 	int i, counter = 0;
 	va_list args;
-
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			switch (format[++i])
-			{
-				case 'c':
-					counter += print_char(va_arg(args, int));
-					break;
-				case 's':
-					counter += print_string(va_arg(args, char*));
-					break;
-				case 'S':
-					counter += print_special_string(va_arg(args, char*));
-					break;
-				case '%':
-					counter += _putchar('%');
-					break;
+	for (i = 0; format[i] != '\0'; i++) {
+		if (format[i] == '%') {
+			switch (format[++i]) {
+				case 'c': counter += print_char(va_arg(args, int)); break;
+				case 's': counter += print_string(va_arg(args, char*)); break;
+				case 'S': counter += print_special_string(va_arg(args, char*)); break;
+				case '%': counter += _putchar('%'); break;
 				case 'd':
-				case 'i':
-					counter += print_int(va_arg(args, int));
-					break;
-				case 'b':
-					counter += print_binary(va_arg(args, unsigned int));
-					break;
-				case 'u':
-					counter += print_unsigned(va_arg(args, unsigned int));
-					break;
-				case 'o':
-					counter += print_octal(va_arg(args, unsigned int));
-					break;
-				case 'x':
-					counter += print_hexadecimal(va_arg(args, unsigned int), 0);
-					break;
-				case 'X':
-					counter += print_hexadecimal(va_arg(args, unsigned int), 1);
-					break;
-				case 'p':
-					counter += print_pointer(va_arg(args, void*));
-					break;
-				default:
-					counter += _putchar('%');
-					counter += _putchar(format[i]);
-					break;
+				case 'i': counter += print_int(va_arg(args, int)); break;
+				case 'b': counter += print_binary(va_arg(args, unsigned int)); break;
+				case 'u': counter += print_unsigned(va_arg(args, unsigned int)); break;
+				case 'o': counter += print_octal(va_arg(args, unsigned int)); break;
+				case 'x': counter += print_hexadecimal(va_arg(args, unsigned int), 0); break;
+				case 'X': counter += print_hexadecimal(va_arg(args, unsigned int), 1); break;
+				case 'p': counter += print_pointer(va_arg(args, void*)); break;
+				default:  counter += _putchar('%'); counter += _putchar(format[i]); break;
 			}
-		}
-		else
-		{
+		} else {
 			counter += _putchar(format[i]);
 		}
 	}
+
 	va_end(args);
-	return (counter);
+	return counter;
 }
 
