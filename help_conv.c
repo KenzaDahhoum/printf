6d01712a3022a81b344e3604b143bf45949afb0c
Yassine <yassinemoussafir31@gmail.com>
2023-11-14 05:31:08 -0800

    betty

--- help_conv.c
+++ help_conv.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
+/**
+ * print_octal - converts an unsigned integer to octal and prints it
+ * @o: unsigned integer to convert
+ * Return: number of characters printed
+ */
 int print_octal(unsigned int o) {
     return decimalToOctal(o);
 }
 
+/**
+ * print_hexadecimal - converts an unsigned integer to hexadecimal (in uppercase or lowercase) and prints it
+ * @x: unsigned integer to convert
+ * @uppercase: flag to determine if the hexadecimal should be printed in uppercase
+ * Return: number of characters printed
+ */
 int print_hexadecimal(unsigned int x, int uppercase) {
     if (uppercase) {
         return decimalToHexadecimalX(x);
@@ -12,11 +23,20 @@ int print_hexadecimal(unsigned int x, int uppercase) {
     }
 }
 
+/**
+ * print_binary - converts an unsigned integer to binary and prints it
+ * @bi: unsigned integer to convert
+ * Return: number of characters printed
+ */
 int print_binary(unsigned int bi) {
     return decimalToBinary(bi);
 }
 
-
+/**
+ * print_reversed_string - reverses a string and prints it
+ * @s: string to reverse and print
+ * Return: number of characters printed
+ */
 int print_reversed_string(char *s) {
     int length = 0;
     int i, count = 0;
@@ -25,19 +45,17 @@ int print_reversed_string(char *s) {
         length++;
     }
 
-    
     for (i = 0; i < length / 2; i++) {
-        
         char temp = s[i];
         s[i] = s[length - i - 1];
         s[length - i - 1] = temp;
     }
 
-    
     for (i = 0; i < length; i++) {
         _putchar(s[i]);
         count++;
     }
 
-    return count; 
+    return count;
 }
+
--- help_functions.c
+++ help_functions.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
+/**
+ * print_unsigned - prints an unsigned integer
+ * @u: unsigned integer to print
+ * Return: number of characters printed
+ */
 int print_unsigned(unsigned int u) {
     char str[30];
     unsigned_itoa(u, str);
     return print_string(str);
 }
+
+/**
+ * print_string - prints a string
+ * @s: string to print
+ * Return: number of characters printed
+ */
 int print_string(char *s) {
     int i, count = 0;
     for (i = 0; s[i] != '\0'; i++) {
@@ -13,6 +24,13 @@ int print_string(char *s) {
     }
     return count;
 }
+
+/**
+ * print_special_string - prints a string, converting non-printable
+ * characters to their ASCII hexadecimal value
+ * @s: string to print
+ * Return: number of characters printed
+ */
 int print_special_string(char *s) {
     int i, count = 0;
     for (i = 0; s[i] != '\0'; i++) {
@@ -28,12 +46,23 @@ int print_special_string(char *s) {
     return count;
 }
 
+/**
+ * print_int - prints an integer
+ * @di: integer to print
+ * Return: number of characters printed
+ */
 int print_int(int di) {
     char str[30];
     _itoa(di, str);
     return print_string(str);
 }
 
+/**
+ * print_pointer - prints a pointer address
+ * @p: pointer to print
+ * Return: number of characters printed
+ */
 int print_pointer(void *p) {
     return print_address(p);
 }
+
